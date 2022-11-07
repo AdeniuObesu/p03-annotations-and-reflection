7eedf646a2ec8a1900d1014146f26598035cb4e2
MOUKHAFI Anass <moukhafi.anass@gmail.com>
2022-11-07 15:02:48 +0100

    Scanned all controller's methods and add them to menus

--- src/org/mql/java/ui/Menu.java
+++ src/org/mql/java/ui/Menu.java
@@ -3,9 +3,13 @@
  */
 package org.mql.java.ui;
 
+import java.lang.reflect.Method;
+
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
+import org.mql.java.annotations.Action;
 import org.mql.java.annotations.Controller;
 
 /**
@@ -24,7 +28,19 @@ public class Menu extends JMenuBar{
 		Controller ca = controller.getClass().getDeclaredAnnotation(Controller.class);
 		if(ca != null) {
 			JMenu menu = new JMenu(ca.value());
-			add(menu); // accroche un menu a un JMenuBar
+			add(menu); // accroche un menu a un JMenu
+			
+			Method methods[] = controller.getClass().getDeclaredMethods();
+			for(Method method : methods) {
+				Action a = method.getDeclaredAnnotation(Action.class);
+				if(a != null) {
+					String name = a.value();
+					if(name.equals(""))
+						name = method.getName();
+					JMenuItem item = new JMenuItem(name);
+					menu.add(item);
+				}
+			}
 		}
 	}
 }
