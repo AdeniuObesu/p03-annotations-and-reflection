fabb4af30549fcccda7833922cf003820124e323
MOUKHAFI Anass <moukhafi.anass@gmail.com>
2022-11-07 16:41:41 +0100

    Implemented ItemListener

--- src/org/mql/java/ui/Menu.java
+++ src/org/mql/java/ui/Menu.java
@@ -3,6 +3,8 @@
  */
 package org.mql.java.ui;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.lang.reflect.Method;
 
 import javax.swing.Icon;
@@ -21,7 +23,7 @@ import org.mql.java.annotations.Controller;
  */
 public class Menu extends JMenuBar{
 	private static final long serialVersionUID = 1l;
-	private final String iconsFolder = "resources/icons"; 
+	private final String iconsFolder = "resources/icons/"; 
 	public Menu() {
 			
 	}
@@ -39,10 +41,32 @@ public class Menu extends JMenuBar{
 					String name = a.value();
 					if(name.equals(""))
 						name = method.getName();
-					ImageIcon icon = new ImageIcon(a.icon());
+					ImageIcon icon = new ImageIcon(iconsFolder + a.icon());
 					JMenuItem item = new JMenuItem(name, icon);
 					menu.add(item);
+					item.addActionListener(new ItemListener(controller, method));
 				}
+			}
+		}
+	}
+	
+	public class ItemListener implements ActionListener {
+		private Object controller;
+		private Method method;
+		
+		public ItemListener(Object controller, Method method) {
+			super();
+			this.controller = controller;
+			this.method = method;
+		}
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			try {
+				String result = (String) method.invoke(controller);
+				System.out.println(">>> Result : " + result);
+			} catch (Exception ex) {
+				System.out.println("Erreur : " +ex.getMessage());
 			}
 		}
 	}
